998e05043f2ddad405d18209d8d035dafbb6ff05
Hanks Audivett <hanksaudivett@gmail.com>
2025-10-23 01:34:14 -0500

    Este es mi primer commit

---
+++ app.py
@@ -0,0 +1,22 @@
+"""Tenemos que tener en cuenta que 'flask' es un modulo para utilizar las herramientas de flask y luego tenemos 'Flask' ya que este es una clase"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__) #__name__ les esta diciendo a Flask en donde debe encontrar archivos (templates, static, etc)
+
+"""Ahora con este codigo estaremos creando la primera ruta den flask, ya que esta nos dice @app.route('/') cuando el usuario ingrese a la URL raiz (http://localhost:5000/), tienes que ejecutar esta función"""
+
+@app.route('/Inicio')
+def inicio():
+    # Ahora retornamos HTML completo
+    # Fíjate cómo puedo usar comillas triples para escribir múltiples líneas
+   return render_template('index.html' )
+
+app.route('/saludo')
+def saludo(nombre):
+    return f"hola {nombre} Bienvenido a nuestra app"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
